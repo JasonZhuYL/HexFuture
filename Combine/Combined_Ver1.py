eb14fd7421b8c17e9cd63e26201b9ed017049f3e
blw219 <boonliang.wong19@imperial.ac.uk>
2022-02-16 12:00:07 +0000

    Combine load,moisture, light and temperature sensor

---
+++ Combine/Combined_Ver1.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python3
+#This script uses a as7262 6 colour spectral scanner from Pimoroni and displays the resulting values.
+#raspberryconnect.com
+
+from as7262 import AS7262
+import sys
+import smbus2
+import time
+import smbus
+
+#----------------------------------------------------------------------------------------------------------------#
+bus = smbus2.SMBus(1)
+
+time.sleep(0.3)
+bus.write_byte(0x40, 0xF3)
+time.sleep(0.3)
+
+time.sleep(0.3)
+bus.write_byte(0x40, 0xF3)
+time.sleep(0.3)
+ 
+# SI7021 address, 0x40 Read data 2 bytes, Temperature
+data0 = bus.read_byte(0x40)
+data1 = bus.read_byte(0x40)
+
+# Convert the data
+humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
+
+# Convert the data and output it
+celsTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
+fahrTemp = celsTemp * 1.8 + 32
+
+#----------------------------------------------------------------------------------------------------------------#
+
+# Register and other configuration values:
+ADS1x15_DEFAULT_ADDRESS        = 0x48
+ADS1x15_POINTER_CONVERSION     = 0x00
+ADS1x15_POINTER_CONFIG         = 0x01
+ADS1x15_POINTER_LOW_THRESHOLD  = 0x02
+ADS1x15_POINTER_HIGH_THRESHOLD = 0x03
+ADS1x15_CONFIG_OS_SINGLE       = 0x8000
+ADS1x15_CONFIG_MUX_OFFSET      = 12
+# Maping of gain values to config register values.
+ADS1x15_CONFIG_GAIN = {
+    2/3: 0x0000,
+    1:   0x0200,
+    2:   0x0400,
+    4:   0x0600,
+    8:   0x0800,
+    16:  0x0A00
+}
+ADS1x15_CONFIG_MODE_CONTINUOUS  = 0x0000
+ADS1x15_CONFIG_MODE_SINGLE      = 0x0100
+# Mapping of data/sample rate to config register values for ADS1015 (faster).
+ADS1015_CONFIG_DR = {
+    128:   0x0000,
+    250:   0x0020,
+    490:   0x0040,
+    920:   0x0060,
+    1600:  0x0080,
+    2400:  0x00A0,
+    3300:  0x00C0
+}
+# Mapping of data/sample rate to config register values for ADS1115 (slower).
+ADS1115_CONFIG_DR = {
+    8:    0x0000,
+    16:   0x0020,
+    32:   0x0040,
+    64:   0x0060,
+    128:  0x0080,
+    250:  0x00A0,
+    475:  0x00C0,
+    860:  0x00E0
+}
+ADS1x15_CONFIG_COMP_WINDOW      = 0x0010
+ADS1x15_CONFIG_COMP_ACTIVE_HIGH = 0x0008
+ADS1x15_CONFIG_COMP_LATCHING    = 0x0004
+ADS1x15_CONFIG_COMP_QUE = {
+    1: 0x0000,
+    2: 0x0001,
+    4: 0x0002
+}
+ADS1x15_CONFIG_COMP_QUE_DISABLE = 0x0003
+
+class ADS1115():
+    """Base functionality for ADS1x15 analog to digital converters."""
+
+    def __init__(self, address=ADS1x15_DEFAULT_ADDRESS, i2c=None, **kwargs):
+        if i2c is None:
+            import Adafruit_GPIO.I2C as I2C
+            i2c = I2C
+        self._device = i2c.get_i2c_device(address, **kwargs)
+        
+    def _data_rate_default(self):
+        # Default from datasheet page 16, config register DR bit default.
+        return 128
+    
+    def _data_rate_config(self, data_rate):
+        if data_rate not in ADS1115_CONFIG_DR:
+            raise ValueError('Data rate must be one of: 8, 16, 32, 64, 128, 250, 475, 860')
+        return ADS1115_CONFIG_DR[data_rate]
+    
+    def _conversion_value(self, low, high):
+        # Convert to 16-bit signed value.
+        value = ((high & 0xFF) << 8) | (low & 0xFF)
+        # Check for sign bit and turn into a negative value if set.
+        if value & 0x8000 != 0:
+            value -= 1 << 16
+        return value
+
+    def read_adc(self, channel, gain=1, data_rate=None):
+        """Read a single ADC channel and return the ADC value as a signed integer
+        result.  Channel must be a value within 0-3.
+        """
+        assert 0 <= channel <= 3, 'Channel must be a value within 0-3!'
+        # Perform a single shot read and set the mux value to the channel plus
+        # the highest bit (bit 3) set.
+        return self._read(channel + 0x04, gain, data_rate, ADS1x15_CONFIG_MODE_SINGLE)
+
+    def _read(self, mux, gain, data_rate, mode):
+        """Perform an ADC read with the provided mux, gain, data_rate, and mode
+        values.  Returns the signed integer result of the read.
+        """
+        config = ADS1x15_CONFIG_OS_SINGLE  # Go out of power-down mode for conversion.
+        # Specify mux value.
+        config |= (mux & 0x07) << ADS1x15_CONFIG_MUX_OFFSET
+        # Validate the passed in gain and then set it in the config.
+        if gain not in ADS1x15_CONFIG_GAIN:
+            raise ValueError('Gain must be one of: 2/3, 1, 2, 4, 8, 16')
+        config |= ADS1x15_CONFIG_GAIN[gain]
+        # Set the mode (continuous or single shot).
+        config |= mode
+        # Get the default data rate if none is specified (default differs between
+        # ADS1015 and ADS1115).
+        if data_rate is None:
+            data_rate = self._data_rate_default()
+        # Set the data rate (this is controlled by the subclass as it differs
+        # between ADS1015 and ADS1115).
+        config |= self._data_rate_config(data_rate)
+        config |= ADS1x15_CONFIG_COMP_QUE_DISABLE  # Disble comparator mode.
+        # Send the config value to start the ADC conversion.
+        # Explicitly break the 16-bit value down to a big endian pair of bytes.
+        self._device.writeList(ADS1x15_POINTER_CONFIG, [(config >> 8) & 0xFF, config & 0xFF])
+        # Wait for the ADC sample to finish based on the sample rate plus a
+        # small offset to be sure (0.1 millisecond).
+        time.sleep(1.0/data_rate+0.0001)
+        # Retrieve the result.
+        result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
+        return self._conversion_value(result[1], result[0])
+
+    def read_adc_difference(self, differential, gain=1, data_rate=None):
+        """Read the difference between two ADC channels and return the ADC value
+        as a signed integer result.  Differential must be one of:
+          - 0 = Channel 0 minus channel 1
+          - 1 = Channel 0 minus channel 3
+          - 2 = Channel 1 minus channel 3
+          - 3 = Channel 2 minus channel 3
+        """
+        assert 0 <= differential <= 3, 'Differential must be a value within 0-3!'
+        # Perform a single shot read using the provided differential value
+        # as the mux value (which will enable differential mode).
+        return self._read(differential, gain, data_rate, ADS1x15_CONFIG_MODE_SINGLE)
+#----------------------------------------------------------------------------------------------------------------#
+
+as7262 = AS7262()
+
+as7262.set_gain(1) # 1, 3.7, 16, 64
+as7262.set_integration_time(10) #1 to 255 x 2.8ms exposure
+#mode 0 - bank 1 only continuous, mode 1 - bank 2 only continuous, mode 2 - both banks continuous, mode 3 - both banks single read 
+as7262.set_measurement_mode(2) #2 all colours continuous
+# as7262.set_illumination_led_current(12.5) #12.5mA 25mA 50mA 100mA
+# as7262.set_illumination_led(1) # led on
+
+def main():
+    adc = ADS1115()
+    GAIN = 2
+    SOIL_GAIN = 1
+    diff = 0
+    mode = 0
+    try:
+        while 1:
+            diff = adc.read_adc_difference(differential = mode,gain = GAIN)
+            soil_sensor = adc.read_adc(2, gain=SOIL_GAIN)
+
+            values = as7262.get_calibrated_values() #get values from scan
+            
+            spec = [float(i) for i in list(values)] #convert results from string to float				
+            sum=0
+            for j in range(len(spec)):
+                sum += spec[j]
+            print('Load Cell Output: {}'.format(diff))
+            print('Moisture Output: {}'.format(soil_sensor))	 
+            print("Light Sensor Output: ",sum)
+            #print("Spec: ",spec)
+    except KeyboardInterrupt:
+            sys.stdout.close()
+            as7262.set_measurement_mode(3) #Switch to scan on demand
+            as7262.set_illumination_led(0) #light off
+
+if __name__ == '__main__':
+    print ("Starting...")
+    sys.stdout = open("train.csv", "w")
+    print ("Relative Humidity is : %.2f %%" %humidity)
+    print ("Temperature in Celsius is : %.2f C" %celsTemp)
+    print ("Temperature in Fahrenheit is : %.2f F" %fahrTemp)
+    main()
