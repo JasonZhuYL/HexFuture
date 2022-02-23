8f39c290cd0b6aacf345787ae0c08ad312bf02cd
a1b2c3zhu <zhuyuliang0817@gmail.com>
2022-02-23 10:57:02 +0000

    LED now working

---
+++ LED/blink.py
@@ -0,0 +1,31 @@
+import time
+import RPi.GPIO as GPIO
+
+# Pin definitions
+led_pin = 22
+
+# Suppress warnings
+GPIO.setwarnings(False)
+
+# Use "GPIO" pin numbering
+GPIO.setmode(GPIO.BCM)
+
+# Set LED pin as output
+GPIO.setup(led_pin, GPIO.OUT)
+#GPIO.setup(10,GPIO.OUT)
+#GPIO.setup(24,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+
+# Blink forever
+print("blinking")
+GPIO.output(led_pin, GPIO.HIGH) # Turn LED on
+#GPIO.output(10,GPIO.HIGH)
+#GPIO.output(24,GPIO.HIGH)
+GPIO.output(23,GPIO.HIGH)
+GPIO.output(22,GPIO.HIGH)
+time.sleep(1)                   # Delay for 1 second
+GPIO.output(led_pin, GPIO.LOW)  # Turn LED off
+time.sleep(1)                   # Delay for 1 second
+
+
