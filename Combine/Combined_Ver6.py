92b9e5824808b9995941ee9862c6e6ffed66d1fe
JasonZhuYL <zhuyuliang0817@gmail.com>
2022-02-23 12:06:05 +0000

    lib update

--- Combine/Combined_Ver6.py
+++ Combine/Combined_Ver6.py
@@ -1,5 +1,29 @@
 import HexLibrary as Hex
 import RPi.GPIO as GPIO
+import smbus2
+import time 
+
+bus = smbus2.SMBus(1)
+
+time.sleep(0.3)
+bus.write_byte(0x40, 0xF3)
+time.sleep(0.3)
+
+time.sleep(0.3)
+bus.write_byte(0x40, 0xF3)
+time.sleep(0.3)
+ 
+# SI7021 address, 0x40 Read data 2 bytes, Temperature
+data0 = bus.read_byte(0x40)
+data1 = bus.read_byte(0x40)
+
+# Convert the data
+humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
+
+# Convert the data and output it
+celsTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
+fahrTemp = celsTemp * 1.8 + 32
+
 
 def main():
     as7262 = Hex.AS7262()
